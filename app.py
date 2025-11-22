10fcf535ac2bfd2afe5b220b1922d58471d75f57
starmarp <starcmendiola@gmail.com>
2025-11-22 15:14:00 -0500

    Add calculation based on user input

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ print("Welcome to my Python program!")
 
 # Ask the user for today's study hours
 hours = input("How many hours did you study today? ")
+
+# Convert input to float
+hours = float(hours)
+
+# Calculate estimated weekly study hours
+weekly_hours = hours * 7
